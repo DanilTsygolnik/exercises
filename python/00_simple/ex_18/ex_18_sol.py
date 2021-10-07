d5fca69753cb8fd60d3774c275866e24b0797de8
Danil Tsygolnik <tsygolnikd@gmail.com>
2021-10-07 06:38:33 +0300

    ex_18 polishing before approval

--- python/00_simple/ex_18/ex_18_sol.py
+++ python/00_simple/ex_18/ex_18_sol.py
@@ -9,15 +9,6 @@ def is_sort(data):
         cnt += 1
     return result
 
-# func(list, index) -> list_rotated
-def get_rotated(data, index):
-    data_mod = data.copy()
-    tmp_head = data[index]
-    data_mod[index] = data[index+1] # mid -> head
-    data_mod[index+1] = data[index+2] # tail -> mid
-    data_mod[index+2] = tmp_head # head -> tail
-    return data_mod
-
 def get_rotated_src(data, index):
     tmp_head = data[index]
     data[index] = data[index+1] # mid -> head
@@ -31,18 +22,11 @@ def MisterRobot(N, data): # N >= 4, len(data) = N
     else:
         cnt = N-3
         while cnt >= 0:
-            print('outlook befor rot --', data)
-            print('ind =', cnt, 'curr_list --', data[cnt:cnt+3])
             curr_max = max(data[cnt:cnt+3])
-            #while curr_max != curr_list[2]:
             while True:
                 if data[cnt+2] == curr_max:
                     break
                 get_rotated_src(data, cnt)
-                #curr_list = get_rotated(curr_list, 0) # !! нужно обновлять исходный список data, когда curr_list[2] == curr_max
-            # test print
-            print('curr_list rotated --', data[cnt:cnt+3])
-            print('outlook --', data)
             if cnt == 0:
                 curr_min = min(data[cnt:cnt+3])
                 if  data[0] == curr_min:
@@ -50,7 +34,3 @@ def MisterRobot(N, data): # N >= 4, len(data) = N
                 else:
                     return False
             cnt -= 1
-
-#print(MisterRobot(7, [1, 3, 4, 5, 6, 2, 7]))
-#print(MisterRobot(4, [3, 2, 4, 1]))
-print(MisterRobot(6, [1, 3, 4, 6, 2, 7]))
--- python/00_simple/ex_18/test_ex_18.py
+++ python/00_simple/ex_18/test_ex_18.py
@@ -9,16 +9,6 @@ class Test_ex_18_sol(unittest.TestCase):
         self.assertTrue(ex_18_sol.is_sort([1, 2, 2, 3]))
         self.assertTrue(ex_18_sol.is_sort([1, 2, 3, 4]))
 
-    def test_get_rotated(self):
-        
-        test_list = [1, 1, 1, 2, 3, 4, 1, 1, 1]
-        rot_one = ex_18_sol.get_rotated(test_list, 3)
-        self.assertEqual(rot_one, [1, 1, 1, 3, 4, 2, 1, 1, 1])
-        rot_two = ex_18_sol.get_rotated(rot_one, 3)
-        self.assertEqual(rot_two, [1, 1, 1, 4, 2, 3, 1, 1, 1])
-        rot_last = ex_18_sol.get_rotated(rot_two, 3)
-        self.assertEqual(rot_last, [1, 1, 1, 2, 3, 4, 1, 1, 1])
-
     def test_get_rotated_src(self):
         
         test_list = [1, 1, 1, 2, 3, 4, 1, 1, 1]
