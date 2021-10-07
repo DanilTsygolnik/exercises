17fcce1fa67c85b8a412fa19d4abd76889146612
Danil Tsygolnik <tsygolnikd@gmail.com>
2021-10-07 20:20:12 +0300

    ex_19, wip -- chains() bug fixed; ShopOLAP() prototype with chains() - suggested test ok

--- python/00_simple/ex_19/test_ex_19.py
+++ python/00_simple/ex_19/test_ex_19.py
@@ -16,16 +16,20 @@ class Test_ex_19_sol(unittest.TestCase):
 
     def test_get_data_templ(self):
 
+        inp_list = ['платье1 5']
+        self.assertEqual(ex_19_sol.get_data_templ(inp_list), [['платье', 1, 5]]) 
         inp_list = ['платье1 5', 'сумка32 2', 'платье1 1', 'сумка23 2', 'сумка128 4']
         self.assertEqual(ex_19_sol.get_data_templ(inp_list), [['платье', 1, 6], 
                                                               ['сумка', 32, 2], 
                                                               ['сумка', 23, 2], 
                                                               ['сумка', 128, 4]])
 
-
     def test_chains(self):
         
         self.assertEqual(ex_19_sol.chains([1, 1, 1, 1]), [[0, 3]])
+        self.assertEqual(ex_19_sol.chains([1, 2, 2, 1]), [[1, 2]])
+        self.assertEqual(ex_19_sol.chains([5, 1, 4, 2, 2]), [[3, 4]])
+        self.assertEqual(ex_19_sol.chains([6, 4, 2, 2]), [[2, 3]])
         self.assertEqual(ex_19_sol.chains([4, 4, 4, 4, 5, 6, 7]), [[0, 3]])
         self.assertEqual(ex_19_sol.chains([1, 2, 3, 4, 4, 4, 4, 5, 6, 7]), [[3, 6]])
         self.assertEqual(ex_19_sol.chains([1, 2, 3, 4, 4, 4, 4]), [[3, 6]])
@@ -34,16 +38,24 @@ class Test_ex_19_sol(unittest.TestCase):
         self.assertEqual(ex_19_sol.chains([4, 4, 4, 4, 5, 6, 7, 8, 8, 8, 8]), [[0, 3], [7, 10]])
 
 
-    #def test_ShopOLAP(self):
-    #    
-    #    # suggested test
-    #    inp_list = ['платье1 5', 
-    #                'сумка32 2', 
-    #                'платье1 1', 
-    #                'сумка23 2', 
-    #                'сумка128 4']
-    #    
-    #    self.assertEqual(ex_19_sol.ShopOLAP(5, inp_list), [['платье', 1, 6], ['сумка', 128, 4], ['сумка', 23, 2], ['сумка', 32, 2]])
+    def test_ShopOLAP(self):
+        
+        # suggested test
+        inp_list = ['платье1 5', 
+                    'сумка32 2', 
+                    'платье1 1', 
+                    'сумка23 2', 
+                    'бумага 2', 
+                    'тетрадь 2', 
+                    'сумка128 4']
+        
+        self.assertEqual(ex_19_sol.ShopOLAP(5, inp_list),   [['платье', 1, 6], 
+                                                            ['сумка', 128, 4], 
+                                                            ['бумага', -1, 2], 
+                                                            ['сумка', 23, 2], 
+                                                            ['сумка', 32, 2], 
+                                                            ['тетрадь', -1, 2]
+                                                            ])
 
     #    # test 1
     #    inp_list = ['платье1 5']
