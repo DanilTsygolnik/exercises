fa9e5971a8de05af1a25cc48d72a28751b7a517b
Danil Tsygolnik <tsygolnikd@gmail.com>
2021-10-07 20:24:43 +0300

    ex_19, wip -- chains() bug fixed; ShopOLAP() prototype - all test ok so far

--- python/00_simple/ex_19/test_ex_19.py
+++ python/00_simple/ex_19/test_ex_19.py
@@ -57,24 +57,32 @@ class Test_ex_19_sol(unittest.TestCase):
                                                             ['тетрадь', -1, 2]
                                                             ])
 
-    #    # test 1
-    #    inp_list = ['платье1 5']
-    #    
-    #    self.assertEqual(ex_19_sol.ShopOLAP(5, inp_list), [['платье1', 5]])
+        # test 1
+        inp_list = ['платье1 5']
+        
+        self.assertEqual(ex_19_sol.ShopOLAP(5, inp_list), [['платье', 1, 5]])
 
-    #    # test 2
-    #    inp_list = ['сумка32 2', 
-    #                'сумка23 2', 
-    #                'сумка128 4']
-    #    
-    #    self.assertEqual(ex_19_sol.ShopOLAP(5, inp_list), [['сумка128', 4], ['сумка23', 2], ['сумка32', 2]])
+        # test 2
+        inp_list = ['сумка32 2', 
+                    'сумка23 2', 
+                    'сумка128 4']
+        
+        self.assertEqual(ex_19_sol.ShopOLAP(5, inp_list),   [
+                                                            ['сумка', 128, 4], 
+                                                            ['сумка', 23, 2], 
+                                                            ['сумка', 32, 2]
+                                                            ])
 
-    #    # test 3
-    #    inp_list = ['сумка32 2', 
-    #                'сумка23 2', 
-    #                'сумка128 2']
-    #    
-    #    self.assertEqual(ex_19_sol.ShopOLAP(5, inp_list), [['сумка23', 2], ['сумка32', 2], ['сумка128', 2]])
+        # test 3
+        inp_list = ['сумка32 2', 
+                    'сумка23 2', 
+                    'сумка128 2']
+        
+        self.assertEqual(ex_19_sol.ShopOLAP(5, inp_list),   [
+                                                            ['сумка', 23, 2], 
+                                                            ['сумка', 32, 2], 
+                                                            ['сумка', 128, 2]
+                                                            ])
 
 
 
