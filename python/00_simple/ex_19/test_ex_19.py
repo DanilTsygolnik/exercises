9e6fc8a0890a46b1f0121669853e0ff8f09c0bfb
Danil Tsygolnik <tsygolnikd@gmail.com>
2021-10-07 17:19:01 +0300

    ex_19, wip -- early solution tests: sorting must be improved

--- python/00_simple/ex_19/test_ex_19.py
+++ python/00_simple/ex_19/test_ex_19.py
@@ -3,16 +3,6 @@ import ex_19_sol
 
 class Test_ex_19_sol(unittest.TestCase):
 
-    def test_chains(self):
-        
-        self.assertEqual(ex_19_sol.chains([1, 1, 1, 1]), [[0, 3]])
-        self.assertEqual(ex_19_sol.chains([4, 4, 4, 4, 5, 6, 7]), [[0, 3]])
-        self.assertEqual(ex_19_sol.chains([1, 2, 3, 4, 4, 4, 4, 5, 6, 7]), [[3, 6]])
-        self.assertEqual(ex_19_sol.chains([1, 2, 3, 4, 4, 4, 4]), [[3, 6]])
-        self.assertEqual(ex_19_sol.chains([1, 2, 3, 4, 4, 4, 4, 5, 6, 7, 8, 8, 8, 8, 9, 10]), [[3, 6], [10, 13]])
-        self.assertEqual(ex_19_sol.chains([1, 2, 3, 4, 4, 4, 4, 5, 6, 7, 8, 8, 8, 8]), [[3, 6], [10, 13]])
-        self.assertEqual(ex_19_sol.chains([4, 4, 4, 4, 5, 6, 7, 8, 8, 8, 8]), [[0, 3], [7, 10]])
-
     def test_get_data_templ(self):
 
         inp_list = ['платье1 5', 'сумка32 2', 'платье1 1', 'сумка23 2', 'сумка128 4']
@@ -20,8 +10,34 @@ class Test_ex_19_sol(unittest.TestCase):
 
     def test_ShopOLAP(self):
         
-        inp_list = ['платье1 5', 'сумка32 2', 'платье1 1', 'сумка23 2', 'сумка128 4']
+        # suggested test
+        inp_list = ['платье1 5', 
+                    'сумка32 2', 
+                    'платье1 1', 
+                    'сумка23 2', 
+                    'сумка128 4']
+        
         self.assertEqual(ex_19_sol.ShopOLAP(5, inp_list), [['платье1', 6], ['сумка128', 4], ['сумка23', 2], ['сумка32', 2]])
+
+        # test 1
+        inp_list = ['платье1 5']
+        
+        self.assertEqual(ex_19_sol.ShopOLAP(5, inp_list), [['платье1', 5]])
+
+        # test 2
+        inp_list = ['сумка32 2', 
+                    'сумка23 2', 
+                    'сумка128 4']
+        
+        self.assertEqual(ex_19_sol.ShopOLAP(5, inp_list), [['сумка128', 4], ['сумка23', 2], ['сумка32', 2]])
+
+        # test 3
+        inp_list = ['сумка32 2', 
+                    'сумка23 2', 
+                    'сумка128 2']
+        
+        self.assertEqual(ex_19_sol.ShopOLAP(5, inp_list), [['сумка23', 2], ['сумка32', 2], ['сумка128', 2]])
+
 
 
 if __name__ == "__main__":
