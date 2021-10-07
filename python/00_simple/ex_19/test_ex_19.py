b68dbbd2e80f6755759087e0eac1c39688e0451b
Danil Tsygolnik <tsygolnikd@gmail.com>
2021-10-07 18:26:53 +0300

    ex_19, wip -- chains() with tests from early stages added

--- python/00_simple/ex_19/test_ex_19.py
+++ python/00_simple/ex_19/test_ex_19.py
@@ -22,16 +22,28 @@ class Test_ex_19_sol(unittest.TestCase):
                                                               ['сумка', 23, 2], 
                                                               ['сумка', 128, 4]])
 
-    def test_ShopOLAP(self):
+
+    def test_chains(self):
         
-        # suggested test
-        inp_list = ['платье1 5', 
-                    'сумка32 2', 
-                    'платье1 1', 
-                    'сумка23 2', 
-                    'сумка128 4']
-        
-        self.assertEqual(ex_19_sol.ShopOLAP(5, inp_list), [['платье', 1, 6], ['сумка', 128, 4], ['сумка', 23, 2], ['сумка', 32, 2]])
+        self.assertEqual(ex_19_sol.chains([1, 1, 1, 1]), [[0, 3]])
+        self.assertEqual(ex_19_sol.chains([4, 4, 4, 4, 5, 6, 7]), [[0, 3]])
+        self.assertEqual(ex_19_sol.chains([1, 2, 3, 4, 4, 4, 4, 5, 6, 7]), [[3, 6]])
+        self.assertEqual(ex_19_sol.chains([1, 2, 3, 4, 4, 4, 4]), [[3, 6]])
+        self.assertEqual(ex_19_sol.chains([1, 2, 3, 4, 4, 4, 4, 5, 6, 7, 8, 8, 8, 8, 9, 10]), [[3, 6], [10, 13]])
+        self.assertEqual(ex_19_sol.chains([1, 2, 3, 4, 4, 4, 4, 5, 6, 7, 8, 8, 8, 8]), [[3, 6], [10, 13]])
+        self.assertEqual(ex_19_sol.chains([4, 4, 4, 4, 5, 6, 7, 8, 8, 8, 8]), [[0, 3], [7, 10]])
+
+
+    #def test_ShopOLAP(self):
+    #    
+    #    # suggested test
+    #    inp_list = ['платье1 5', 
+    #                'сумка32 2', 
+    #                'платье1 1', 
+    #                'сумка23 2', 
+    #                'сумка128 4']
+    #    
+    #    self.assertEqual(ex_19_sol.ShopOLAP(5, inp_list), [['платье', 1, 6], ['сумка', 128, 4], ['сумка', 23, 2], ['сумка', 32, 2]])
 
     #    # test 1
     #    inp_list = ['платье1 5']
