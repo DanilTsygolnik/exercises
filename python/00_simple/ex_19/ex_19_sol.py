0ded5e2fe85e253a9bf754a5591ecaccf5b48cf4
Danil Tsygolnik <tsygolnikd@gmail.com>
2021-10-08 10:33:37 +0300

    ex_19, wip -- solution reworked, suggested test ok - ready for approval

--- python/00_simple/ex_19/ex_19_sol.py
+++ python/00_simple/ex_19/ex_19_sol.py
@@ -1,21 +1,3 @@
-def get_item_name_and_num(orig_name):
-    index = 0
-    numbers = []
-    item_data = []
-    for i in range(10):
-        numbers += [str(i)]
-    for i in orig_name:
-        if i in numbers:
-            item_data.append(orig_name[0:index])
-            item_data.append(int(orig_name[index:]))
-            break
-        index += 1
-    if item_data == []:
-        item_data.append(orig_name)
-        item_data.append(-1)
-    return item_data
-     
-
 def chains(L):
     coord = []
     chain_head = None
@@ -36,8 +18,6 @@ def chains(L):
         cnt += 1
     return coord
 
-
-
 def get_data_templ(data_mess):
     items = {}
     for i in data_mess:
@@ -49,29 +29,25 @@ def get_data_templ(data_mess):
 
     data_templ = []
     for i in items.keys():
-        name_num = get_item_name_and_num(i)
-        data_templ.append([name_num[0], name_num[1], items[i]])
+        data_templ.append([i, items[i]])
     return data_templ
 
 def ShopOLAP(N, data_mess):
-    amount_sorted = sorted(get_data_templ(data_mess), reverse=True, key=lambda x: x[2])
+
+    amount_sorted = sorted(get_data_templ(data_mess), reverse=True, key=lambda x: x[1])
     
     amount_list = []
     for i in amount_sorted:
-        amount_list.append(i[2])
+        amount_list.append(i[1])
 
     coord = chains(amount_list)
 
     for i in coord:
-        curr_sort = sorted(amount_sorted[i[0]:i[1]+1], key=lambda x: (x[0], x[1]))
+        curr_sort = sorted(amount_sorted[i[0]:i[1]+1], key=lambda x: x[0])
         amount_sorted = amount_sorted[:i[0]] + curr_sort + amount_sorted[i[1]+1:]
 
     data_organized = []
     for i in amount_sorted:
-        curr_str = i[0]
-        if i[1] == -1:
-            curr_str += ' ' + str(i[2])
-        else:
-            curr_str += str(i[1]) + ' ' + str(i[2])
+        curr_str = i[0] + ' ' + str(i[1])
         data_organized.append(curr_str) 
     return data_organized
