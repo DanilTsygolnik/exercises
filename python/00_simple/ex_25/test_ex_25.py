5416ed245c80497132f3cb2e0677ede111e2c20e
Danil Tsygolnik <tsygolnikd@gmail.com>
2021-10-15 07:54:13 +0300

    ex_24, finished in 17 hours 15 minutes.
    == Stages ==
    Solution on paper - 11 hours 30 minutes;
    Programming the solution - 2 hours 30 minutes;
    Writing tests and debugging - 3 hours 15 minutes;

---
+++ python/00_simple/ex_25/test_ex_25.py
@@ -0,0 +1,12 @@
+import unittest
+import ex_xx_sol
+
+class Test_ex_xx_sol(unittest.TestCase):
+
+    def test_func(self):
+        
+        self.assertEqual(ex_xx_sol.func(), )
+
+
+if __name__ == "__main__":
+    unittest.main()
