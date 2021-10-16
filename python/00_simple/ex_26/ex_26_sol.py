eebd22255e88e4d14a0835d47946e15fcacfb4d0
Danil Tsygolnik <tsygolnikd@gmail.com>
2021-10-16 07:38:54 +0300

    ex_26, wip -- chain_is_there() - ready, tests ok

--- python/00_simple/ex_26/ex_26_sol.py
+++ python/00_simple/ex_26/ex_26_sol.py
@@ -10,13 +10,13 @@
 Т.е. строку вида '===123abcd' на вход функция точно не получит.
 
 Функция возвращает True при условии:
-в строке между каждой парой чисел, сумма которых равна 10, есть ровно три символа "=".
+в строке между каждой парой чисел, сумма которых равна 10, есть цепь из трех символов "=".
 
-"axxb6===4xaf5===eee5" => true
-"5==ooooooo=5=5" => false
-"abc=7==hdjs=3gg1=======5" => true
-"aaS=8" => false
-"9===1===9===1===9" => true
+"axxb6===4xaf5===eee5" => true (2 пары, три знака '=' в 2/2)
+"5==ooooooo=5=5" => false (2 пары, три знака '=' в 1/2)
+"abc=7==hdjs=3gg1=======5" => true (1 пара, три знака '=' в 1/1)
+"aaS=8" => false (0 пар)
+"9===1===9===1===9" => true (4 пары, три знака '=' в 4/4)
 """
 
 
@@ -43,3 +43,26 @@ def get_pairs(string):
                 head = index_cnt
         index_cnt += 1
     return pairs
+
+def chain_is_there(target, location, target_cnt=3):
+    """
+    Проверка на присутствие заданного кол-ва target'ов в строке.
+    Функция возвращает True, если в интервале заданное кол-во найдено.
+    По умолчанию, в соответствии с заданием, target_cnt=3.
+    """
+    cnt = 0
+    for i in location:
+        if i == target:
+            cnt += 1
+            if cnt == target_cnt:
+                return True
+    return False
+
+#def white_walkers(string):
+#    """Функция по заданию"""
+#
+#    numbers_pairs = get_pairs(string)
+#    for i in numbers_pairs:
+#        str_btw_numbers = string[i[0]:i[1]]
+#    ...
+#    return False
--- python/00_simple/ex_26/test_ex_26.py
+++ python/00_simple/ex_26/test_ex_26.py
@@ -16,9 +16,52 @@ class Test_ex_26_sol(unittest.TestCase):
         self.assertEqual(ex_26_sol.get_pairs("axxb6===4x"), [[5,8]])
         self.assertEqual(ex_26_sol.get_pairs("9===1===9="), [[1,4], [5,8]])
 
+    def test_chain_is_there(self):
+        
+        self.assertFalse(ex_26_sol.chain_is_there(target="=", location=""))
+        self.assertFalse(ex_26_sol.chain_is_there(target="=", location="abcd"))
+        self.assertFalse(ex_26_sol.chain_is_there(target="=", location="==ooo"))
+
+        self.assertTrue(ex_26_sol.chain_is_there(target="=", location="axxb6===4x"))
+        self.assertTrue(ex_26_sol.chain_is_there(target="=", location="==axxb6===4x"))
+
     #def test_func(self):
-    #    
-    #    self.assertEqual(ex_26_sol.func(), )
+
+
+    #    #      |+++| -- chain
+    #    # "axxb6===4xaf5===eee5" => true
+
+    #    #  |++-      +|+| --> no chain 
+    #    # "5==ooooooo=5=5" => false
+
+    #    #      no chain    chain
+    #    #      |++-   +|  |+++    |
+    #    # "abc=7==hdjs=3gg1=======5" => true
+
+    #    # "aaS=8" => false
+
+    #    #  chain
+    #    #  |+++|
+    #    # "9===1===9===1===9" => true
+
+    #    self.assertTrue(ex_26_sol.white_walkers("axxb6===4xaf5===eee5"))
+    #    self.assertTrue(ex_26_sol.white_walkers("abc=7==hdjs=3gg1=======5"))
+    #    self.assertTrue(ex_26_sol.white_walkers("9===1===9===1===9"))
+    #    self.assertTrue(ex_26_sol.white_walkers("axxb6===4xaf5===eee6"))
+    #    self.assertTrue(ex_26_sol.white_walkers("axxb6===4"))
+    #    self.assertTrue(ex_26_sol.white_walkers("6===4"))
+    #    self.assertTrue(ex_26_sol.white_walkers("6===4xa===f5===eee6==afada=4"))
+
+    #    self.assertFalse(ex_26_sol.white_walkers("5==ooooooo=5=5"))
+    #    self.assertFalse(ex_26_sol.white_walkers("aaS=8"))
+    #    self.assertFalse(ex_26_sol.white_walkers("9abc1===9===1===9"))
+    #    self.assertFalse(ex_26_sol.white_walkers(""))
+    #    self.assertFalse(ex_26_sol.white_walkers("9==1===9===1===9"))
+    #    self.assertFalse(ex_26_sol.white_walkers("9====1===9===1===9"))
+    #    self.assertFalse(ex_26_sol.white_walkers("axxb6===4xaf6===eee5"))
+    #    self.assertFalse(ex_26_sol.white_walkers("6====4"))
+    #    self.assertFalse(ex_26_sol.white_walkers("6==4"))
+    #    self.assertFalse(ex_26_sol.white_walkers("6===4xa===f5===eee6==afada4"))
 
 
 if __name__ == "__main__":
