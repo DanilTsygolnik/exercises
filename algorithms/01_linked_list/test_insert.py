0bb54370ee752bf66ea5a6522487172bb291a035
Danil Tsygolnik <tsygolnikd@gmail.com>
2021-10-25 16:08:23 +0300

    algo, linked list, wip -- insert() tests: setUp ok

--- algorithms/01_linked_list/test_insert.py
+++ algorithms/01_linked_list/test_insert.py
@@ -4,45 +4,45 @@ import linked_list
 
 class TestLinkedList(unittest.TestCase):
     
-    def test_linked_list_len(self):
-        memory_slots = (8*ctypes.py_object)()
-        
-        memory_slots[0] = linked_list.Node(0) # node 1
-        memory_slots[1] = linked_list.Node(0) # node 2
-        memory_slots[2] = linked_list.Node(0) # node 3
-        memory_slots[3] = linked_list.Node(1) # node 4
-        memory_slots[4] = linked_list.Node(0) # node 5
-        memory_slots[5] = linked_list.Node(0) # node 6
-        memory_slots[6] = linked_list.Node(0) # node 7
-        
-        # newNode
-        memory_slots[7] = linked_list.Node(9) # node 8
+    # Cases for the tests:
+    # _case_1 список пустой, afterNode=None
+    # _case_2 список пустой, afterNode!=None
+    # _case_3 список не пустой, afterNode=None
+    # _case_4 список не пустой, afterNode!=None
+    
+    def setUp(self):
 
-        s_list = linked_list.LinkedList()
-        for i in range(7):
-            s_list.add_in_tail(memory_slots[i])
-        
-        newNode = memory_slots[7]
+        self.memory_slots = (4*ctypes.py_object)()
+        self.memory_slots[0] = linked_list.Node(0) # node 1
+        self.memory_slots[1] = linked_list.Node(0) # node 2
+        self.memory_slots[2] = linked_list.Node(0) # node 3
+        self.memory_slots[3] = linked_list.Node(1) # newNode
+        self.s_list = linked_list.LinkedList()
+        for i in range(3):
+            self.s_list.add_in_tail(self.memory_slots[i])
+        self.newNode = self.memory_slots[3]
 
-        # test 1 - insert as a new head
-        s_list.insert(None, newNode)
-        ref_list = [9, 0, 0, 0, 1, 0, 0, 0]
-        self.assertEqual(s_list.print_all_nodes(as_list=True), ref_list)
-        s_list.delete(9, True)
+    def test_check_setup(self):
+        val_list = []
+        for i in self.memory_slots:
+            val_list.append(str(i.value))
+        values_str = "".join(val_list)
+        self.assertEqual(values_str, "0001") # memory_slots fill check
 
-        # test 2 - insert after random node
-        afterNode = memory_slots[3]
-        s_list.insert(afterNode, newNode)
-        ref_list = [0, 0, 0, 1, 9, 0, 0, 0]
-        self.assertEqual(s_list.print_all_nodes(as_list=True), ref_list)
-        s_list.delete(9, True)
+        self.assertEqual(self.newNode.value, 1) # newNode check
+        self.assertIsNone(self.newNode.next) # newNode check
 
-        # test 3 - insert as a new tail
-        afterNode = s_list.tail
-        s_list.insert(afterNode, newNode)
-        ref_list = [0, 0, 0, 1, 0, 0, 0, 9]
-        self.assertEqual(s_list.print_all_nodes(as_list=True), ref_list)
-        s_list.delete(9, True)
+        s_list_val_ref = [0,0,0]
+        self.assertEqual(self.s_list.print_all_nodes(as_list=True), s_list_val_ref) # s_list fill check
+
+
+    #def test_linked_list_len(self):
+
+    #    # test 1 - insert as a new head
+    #    s_list.insert(None, newNode)
+    #    ref_list = [9, 0, 0, 0, 1, 0, 0, 0]
+    #    self.assertEqual(s_list.print_all_nodes(as_list=True), ref_list)
+    #    s_list.delete(9, True)
 
 
 if __name__ == "__main__":
--- algorithms/01_linked_list/tests.py
+++ algorithms/01_linked_list/tests.py
@@ -138,21 +138,21 @@ class TestLinkedList(unittest.TestCase):
     def test_linked_list_len(self):
         memory_slots = (13*ctypes.py_object)()
         
-        memory_slots[0] = linked_list.Node(0) # node 1
-        memory_slots[1] = linked_list.Node(0) # node 2
-        memory_slots[2] = linked_list.Node(0) # node 3
-        memory_slots[3] = linked_list.Node(1) # node 4
-        memory_slots[4] = linked_list.Node(0) # node 5
-        memory_slots[5] = linked_list.Node(0) # node 6
-        memory_slots[6] = linked_list.Node(0) # node 7
-        memory_slots[7] = linked_list.Node(1) # node 8
-        memory_slots[8] = linked_list.Node(1) # node 9
-        memory_slots[9] = linked_list.Node(1) # node 10
-        memory_slots[10] = linked_list.Node(0) # node 11
-        memory_slots[11] = linked_list.Node(0) # node 12
-        memory_slots[12] = linked_list.Node(0) # node 13
+        memory_slots[0] = Node(0) # node 1
+        memory_slots[1] = Node(0) # node 2
+        memory_slots[2] = Node(0) # node 3
+        memory_slots[3] = Node(1) # node 4
+        memory_slots[4] = Node(0) # node 5
+        memory_slots[5] = Node(0) # node 6
+        memory_slots[6] = Node(0) # node 7
+        memory_slots[7] = Node(1) # node 8
+        memory_slots[8] = Node(1) # node 9
+        memory_slots[9] = Node(1) # node 10
+        memory_slots[10] = Node(0) # node 11
+        memory_slots[11] = Node(0) # node 12
+        memory_slots[12] = Node(0) # node 13
 
-        s_list = linked_list.LinkedList()
+        s_list = LinkedList()
         for i in range(13):
             s_list.add_in_tail(memory_slots[i])
         
