45803f6e94a546febdb35de5fac946b5de5c7c3c
Danil Tsygolnik <tsygolnikd@gmail.com>
2021-10-25 14:46:22 +0300

    Files management before fixing insert()

--- algorithms/01_linked_list/linked_list.py
+++ algorithms/01_linked_list/linked_list.py
@@ -1,33 +1,34 @@
+# pylint: disable=c0103,c0114,r0903
 class Node:
-
+    """Класс для создания узлов связного списка"""
     def __init__(self, v):
         self.value = v
         self.next = None
 
 class LinkedList:
-
+    """Класс для работы со связными списками"""
     def __init__(self):
         self.head = None
         self.tail = None
 
     def add_in_tail(self, item):
+        """Метод добавления нового узла в конец связного списка"""
         if self.head is None:
             self.head = item
         else:
             self.tail.next = item
         self.tail = item
 
-   #def print_all_nodes(self):
-   #    node = self.head
-   #    while node != None:
-   #        print(node.value)
-   #        node = node.next
-
     def print_all_nodes(self, as_list=False):
+        """
+        Метод вывода значений всех узлов связного списка
+        По умолчанию выводит на экран значение каждого узла
+        При as_list=True возвращает список значений узлов
+        """
         node = self.head
         nodes_values = []
-        while node != None:
-            if as_list == False:
+        while node is not None:
+            if as_list is False:
                 print(node.value)
             nodes_values.append(node.value)
             node = node.next
@@ -35,6 +36,7 @@ class LinkedList:
             return nodes_values
 
     def find(self, val):
+        """Метод возвращает первый найденный по значению val узел"""
         node = self.head
         while node is not None:
             if node.value == val:
@@ -43,25 +45,18 @@ class LinkedList:
         return None
 
     # ++ approved
-    def find_all(self, val):
-        results = None
-        node = self.head
-        while node is not None:
-            if node.value == val:
-                if results == None:
-                    results = []
-                results.append(node)
-            node = node.next
-        return results
-
-    # ready for approval
-    def delete(self, val, remove_all=True):
+    def delete(self, val, remove_all=False):
+        """
+        Метод удаления узлов по заданному значению val
+        По умолчанию из списка удаляется только первый найденный узел
+        При remove_all=True будут удалены все найденные узлы
+        """
         curr_node = self.head
         prev_node = None
         while curr_node is not None:
             if curr_node.value == val:
                 if curr_node is self.tail:
-                    if prev_node == None:
+                    if prev_node is None:
                         self.clean()
                         break
                     prev_node.next = None
@@ -71,10 +66,10 @@ class LinkedList:
                         self.head = curr_node.next
                     else:
                         prev_node.next = curr_node.next
-                        if remove_all == False:
+                        if remove_all is False:
                             break
                 else:
-                    if remove_all == False:
+                    if remove_all is False:
                         if prev_node is None:
                             self.head = curr_node.next
                         else:
@@ -84,13 +79,26 @@ class LinkedList:
                 prev_node = curr_node
             curr_node = curr_node.next
 
-    # ready for approval
+    # ++ approved
+    def find_all(self, val):
+        """Метод возвращает список всех узлов со значением val"""
+        results = []
+        node = self.head
+        while node is not None:
+            if node.value == val:
+                results.append(node)
+            node = node.next
+        return results
+
+    # ++ approved
     def clean(self):
+        """Метод удаления всех узлов"""
         self.head = None
         self.tail = None
- 
-    # ready for approval
+
+    # ++ approved
     def len(self):
+        """Метод возвращает кол-во узлов в связном списке"""
         cnt = 0
         node = self.head
         while node is not None:
@@ -100,7 +108,8 @@ class LinkedList:
 
     # ready for approval
     def insert(self, afterNode, newNode):
-        if afterNode is None:
+        """Вставка нового узла newNode после заданного afterNode"""
+        if (self.head is None) or (afterNode is None):
             newNode.next = self.head
             self.head = newNode
         else:
--- algorithms/01_linked_list/tests.py
+++ algorithms/01_linked_list/tests.py
@@ -5,7 +5,7 @@ from linked_list import Node
 
 class TestLinkedList(unittest.TestCase):
     
-    def test_linked_list_delete(self):
+    def test_delete(self):
         
         # логика тестов
         # -------------
@@ -134,6 +134,31 @@ class TestLinkedList(unittest.TestCase):
             
             # для каждого случая (после if, но в цикле for)
             self.assertEqual(nodes_val_after_del, nodes_val_ref)
+
+    def test_linked_list_len(self):
+        memory_slots = (13*ctypes.py_object)()
+        
+        memory_slots[0] = linked_list.Node(0) # node 1
+        memory_slots[1] = linked_list.Node(0) # node 2
+        memory_slots[2] = linked_list.Node(0) # node 3
+        memory_slots[3] = linked_list.Node(1) # node 4
+        memory_slots[4] = linked_list.Node(0) # node 5
+        memory_slots[5] = linked_list.Node(0) # node 6
+        memory_slots[6] = linked_list.Node(0) # node 7
+        memory_slots[7] = linked_list.Node(1) # node 8
+        memory_slots[8] = linked_list.Node(1) # node 9
+        memory_slots[9] = linked_list.Node(1) # node 10
+        memory_slots[10] = linked_list.Node(0) # node 11
+        memory_slots[11] = linked_list.Node(0) # node 12
+        memory_slots[12] = linked_list.Node(0) # node 13
+
+        s_list = linked_list.LinkedList()
+        for i in range(13):
+            s_list.add_in_tail(memory_slots[i])
+        
+        self.assertEqual(s_list.len(), 13)
+        s_list.clean()
+        self.assertEqual(s_list.len(), 0)
 
 if __name__ == "__main__":
     unittest.main()
