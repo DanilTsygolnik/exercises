5fe18ae08fa905f7bdc534e8b92e45b1f23e1f74
Danil Tsygolnik <tsygolnikd@gmail.com>
2021-10-28 15:26:05 +0300

    codewars-3, wip -- lovefunc(): solution ready, tests ok

--- code_practice/01_codewars/3/sol.py
+++ code_practice/01_codewars/3/sol.py
@@ -19,4 +19,8 @@ Write a function that will take the number of petals of each flower...
 def lovefunc(flower1, flower2):
     assert type(flower1)==type(1)
     assert type(flower2)==type(1)
-    pass
+    assert flower1!=0 and flower2!=0
+
+    if flower1%2+flower2%2==1:
+        return True
+    return False
--- code_practice/01_codewars/3/test.py
+++ code_practice/01_codewars/3/test.py
@@ -10,8 +10,18 @@ class Test(unittest.TestCase):
             lovefunc(1, '1')
         with self.assertRaises(AssertionError):
             lovefunc('1', '1')
+        with self.assertRaises(AssertionError):
+            lovefunc(0,1)
+        with self.assertRaises(AssertionError):
+            lovefunc(1,0)
+        with self.assertRaises(AssertionError):
+            lovefunc(0,0)
 
-    def test_(self):
+    def test_lovefunc(self):
+        self.assertTrue(lovefunc(1,2))
+        self.assertTrue(lovefunc(2,1))
+        self.assertFalse(lovefunc(2,2))
+        self.assertFalse(lovefunc(1,1))
         pass
 
 if __name__=="__main__":
