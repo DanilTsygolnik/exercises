b6c979f4bc0af916e4e42243bc7f8092e6bc40ca
Danil Tsygolnik <tsygolnikd@gmail.com>
2021-10-28 15:32:17 +0300

    codewars-3 - finished in 27 minutes

--- code_practice/01_codewars/3/sol.py
+++ code_practice/01_codewars/3/sol.py
@@ -14,6 +14,8 @@ They are in love if:
 Write a function that will take the number of petals of each flower...
 ...and return true if they are in love and false if they aren't.
 
+---
+https://www.codewars.com/kata/555086d53eac039a2a000083/solutions/python
 """
 
 def lovefunc(flower1, flower2):
@@ -23,3 +25,6 @@ def lovefunc(flower1, flower2):
     if flower1%2+flower2%2==1:
         return True
     return False
+
+def lovefunc_best_practice(flower1, flower2):
+    return (flower1+flower2)%2
