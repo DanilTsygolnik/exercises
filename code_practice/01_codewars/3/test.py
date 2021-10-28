d571ec2b24450aa37fec07397bcf0dac4f285922
Danil Tsygolnik <tsygolnikd@gmail.com>
2021-10-28 15:28:49 +0300

    codewars-3, wip -- removed assertion for 0 to pass suggested tests; ready to submit

--- code_practice/01_codewars/3/test.py
+++ code_practice/01_codewars/3/test.py
@@ -10,19 +10,17 @@ class Test(unittest.TestCase):
             lovefunc(1, '1')
         with self.assertRaises(AssertionError):
             lovefunc('1', '1')
-        with self.assertRaises(AssertionError):
-            lovefunc(0,1)
-        with self.assertRaises(AssertionError):
-            lovefunc(1,0)
-        with self.assertRaises(AssertionError):
-            lovefunc(0,0)
 
     def test_lovefunc(self):
         self.assertTrue(lovefunc(1,2))
         self.assertTrue(lovefunc(2,1))
         self.assertFalse(lovefunc(2,2))
         self.assertFalse(lovefunc(1,1))
-        pass
+        # suggested
+        self.assertIs(lovefunc(1,4), True)
+        self.assertIs(lovefunc(2,2), False)
+        self.assertIs(lovefunc(0,1), True)
+        self.assertIs(lovefunc(0,0), False)
 
 if __name__=="__main__":
     unittest.main()
