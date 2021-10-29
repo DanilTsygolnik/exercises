4b3a45ff6c13140140cedf5310b4f08060d87f57
Danil Tsygolnik <tsygolnikd@gmail.com>
2021-10-29 11:25:15 +0300

    codewars-9 - finished in 15 minutes

--- code_practice/01_codewars/9/sol.py
+++ code_practice/01_codewars/9/sol.py
@@ -18,6 +18,4 @@ https://www.codewars.com/kata/55685cd7ad70877c23000102/
 def make_negative(num):
     if not isinstance(num, (int, float)):
         raise TypeError
-    if num > 0:
-        return -num
-    return num
+    return -abs(num)
