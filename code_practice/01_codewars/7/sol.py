3c986c1643b48f23093330f89b2a551f493234a7
Danil Tsygolnik <tsygolnikd@gmail.com>
2021-10-29 09:57:55 +0300

    codewars-7 - finished in 15 minutes

--- code_practice/01_codewars/7/sol.py
+++ code_practice/01_codewars/7/sol.py
@@ -8,15 +8,17 @@ Arrary plus array
 There are 2 arrays containing only integer numbers.
 Calculate the sum of all their elements. Return integer.
 
-
+---
+https://www.codewars.com/kata/5a2be17aee1aaefe2a000151/
 """
-def arr_elem_sum(arr):
-    assert isinstance(arr, type([]))
-    elem_sum = 0
-    for i in arr:
-        assert isinstance(i, type(1))
-        elem_sum += i
-    return elem_sum
-
 def array_plus_array(arr1,arr2):
-    return arr_elem_sum(arr1) + arr_elem_sum(arr2)
+    def arr_check(arr):
+        """Checking if input is correct"""
+        assert isinstance(arr, type([]))
+        for i in arr:
+            assert isinstance(i, type(1))
+    arr_check(arr1)
+    arr_check(arr2)
+
+   # best practice for this task
+    return sum(arr1) + sum(arr2)
--- code_practice/01_codewars/7/test.py
+++ code_practice/01_codewars/7/test.py
@@ -1,20 +1,15 @@
 import unittest
-from sol import arr_elem_sum
 from sol import array_plus_array
 
 class Test(unittest.TestCase):
 
-    def test_arr_elem_sum(self):
-        with self.assertRaises(AssertionError):
-            arr_elem_sum('123')
-        with self.assertRaises(AssertionError):
-            arr_elem_sum([1,2,'3',4])
-
-        self.assertEqual(arr_elem_sum([]), 0)
-        self.assertEqual(arr_elem_sum([1]), 1)
-        self.assertEqual(arr_elem_sum([1,1]), 2)
-
     def test_array_plus_array(self):
+        with self.assertRaises(AssertionError):
+            array_plus_array([],'123')
+        with self.assertRaises(AssertionError):
+            array_plus_array([], [1,2,'3',4])
+
+        self.assertEqual(array_plus_array([], []), 0)
         self.assertEqual(array_plus_array([], [1]), 1)
         self.assertEqual(array_plus_array([1], [1]), 2)
 
