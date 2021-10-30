eff4bf6d0ca3d9793656743dbc62e97ceb632c03
Danil Tsygolnik <tsygolnikd@gmail.com>
2021-10-30 07:54:14 +0300

    algo, doubly linked list, wip: find_bin(): tests updated; done

--- algorithms/02_doubly_linked_list/test_main_methods.py
+++ algorithms/02_doubly_linked_list/test_main_methods.py
@@ -3,44 +3,59 @@ import unittest
 from doubly_linked_list import LinkedList2
 from doubly_linked_list import Node
 
+def get_linked(nodes):
+    s_list = LinkedList2()
+    for i in nodes:
+        s_list.add_in_tail(i)
+    return s_list
+
 class TestFind(unittest.TestCase):
-    def setUp(self):
-        self.nodes = [Node(0), Node(0), Node(1), Node(0)]
-        self.s_list = LinkedList2()
-        for i in self.nodes:
-            self.s_list.add_in_tail(i)
 
     def test_setup_check(self):
-        self.assertEqual(self.s_list.get_all_nodes(), self.nodes)
-        self.assertEqual(self.s_list.get_all_nodes(True), [0,0,1,0])
+        nodes = [Node(0), Node(0), Node(1), Node(0)]
+        s_list = get_linked(nodes)
+        self.assertEqual(s_list.get_all_nodes(), nodes)
+        self.assertEqual(s_list.get_all_nodes(True), [0,0,1,0])
 
     def test_empty(self):
-        self.s_list.clean()
-        self.assertEqual(self.s_list.find_bin(1), None)
+        nodes = []
+        s_list = get_linked(nodes)
+        self.assertEqual(s_list.find_bin(1), None)
 
-    def test_one_node(self):
-        self.s_list.clean()
-        node = Node(1)
-        self.s_list.add_in_tail(node)
-        self.assertEqual(self.s_list.find_bin(1), node)
-        node = Node(0)
-        self.s_list.clean()
-        self.s_list.add_in_tail(node)
-        self.assertEqual(self.s_list.find_bin(1), None)
+    def test_one_node_pos(self):
+        nodes = [Node(1)]
+        s_list = get_linked(nodes)
+        self.assertEqual(s_list.find_bin(1), nodes[0])
+
+    def test_one_node_neg(self):
+        nodes = [Node(0)]
+        s_list = get_linked(nodes)
+        self.assertEqual(s_list.find_bin(1), None)
 
     def test_even_nodes_num1(self):
-        self.assertEqual(self.s_list.find_bin(1), self.nodes[2])
+        nodes = [Node(0), Node(0), Node(1), Node(0)]
+        s_list = get_linked(nodes)
+        self.assertEqual(s_list.find_bin(1), nodes[2])
 
     def test_even_nodes_num2(self):
-        self.nodes = [Node(0), Node(1), Node(0), Node(0)]
-        self.s_list = LinkedList2()
-        for i in self.nodes:
-            self.s_list.add_in_tail(i)
-        self.assertEqual(self.s_list.find_bin(1), self.nodes[1])
+        nodes = [Node(0), Node(1), Node(0), Node(0)]
+        s_list = get_linked(nodes)
+        self.assertEqual(s_list.find_bin(1), nodes[1])
 
     def test_odd_nodes_num1(self):
-        self.s_list.add_in_tail(Node(0))
-        self.assertEqual(self.s_list.find_bin(1), self.nodes[2])
+        nodes = [Node(0), Node(0), Node(0), Node(1), Node(0), Node(0), Node(0)]
+        s_list = get_linked(nodes)
+        self.assertEqual(s_list.find_bin(1), nodes[3])
+
+    def test_odd_nodes_num2(self):
+        nodes = [Node(0), Node(0), Node(1), Node(0), Node(0), Node(0), Node(0)]
+        s_list = get_linked(nodes)
+        self.assertEqual(s_list.find_bin(1), nodes[2])
+
+    def test_odd_nodes_num2(self):
+        nodes = [Node(0), Node(0), Node(0), Node(0), Node(1), Node(0), Node(0)]
+        s_list = get_linked(nodes)
+        self.assertEqual(s_list.find_bin(1), nodes[4])
 
 if __name__=="__main__":
     unittest.main()
