a67b777b558ac6d323a7b3a9975b2ad3bf094582
Danil Tsygolnik <tsygolnikd@gmail.com>
2021-11-04 18:22:30 +0300

    algo, stack, wip: task 1 finished

--- algorithms/04_stack/stack.py
+++ algorithms/04_stack/stack.py
@@ -1,5 +1,3 @@
-import ctypes
-
 class Stack:
 
     def __init__(self):
@@ -8,3 +6,21 @@ class Stack:
     def size(self):
         """Return the number of elements in stack"""
         return len(self.stack)
+
+    def push(self, new_elem):
+        """Add a new element to stack"""
+        self.stack.append(new_elem)
+
+    def peek(self):
+        """Return value of the head element"""
+        if self.size() != 0:
+            return self.stack[-1]
+        return None
+
+    def pop(self):
+        """Return value of the head and remove it from stack"""
+        if self.size() != 0:
+            curr_elem = self.stack[-1]
+            self.stack = self.stack[:-1]
+            return curr_elem
+        return None
--- algorithms/04_stack/test_stack.py
+++ algorithms/04_stack/test_stack.py
@@ -3,8 +3,30 @@ from stack import Stack
 
 class TestStack(unittest.TestCase):
 
+    def setUp(self):
+        self.stack = Stack()
+        self.stack.stack = [1,2,3,4]
+
     def test_size(self):
-        stack = Stack()
-        self.assertEqual(stack.size(), 0)
-        stack.stack = [1,2,3,4]
-        self.assertEqual(stack.size(), 4)
+        self.assertEqual(self.stack.size(), 4)
+        self.stack.stack = []
+        self.assertEqual(self.stack.size(), 0)
+
+    def test_push(self):
+        self.stack.push(5)
+        self.assertEqual(self.stack.size(), 5)
+        self.assertEqual(self.stack.stack, [1,2,3,4,5])
+
+    def test_peek(self):
+        self.assertEqual(self.stack.peek(), 4)
+        self.stack.stack = []
+        self.assertTrue(self.stack.peek() is None)
+
+    def test_pop(self):
+        for i in range(4,0,-1):
+            self.assertEqual(self.stack.size(), i)
+            self.assertEqual(self.stack.pop(), i)
+        self.assertTrue(self.stack.peek() is None)
+
+if __name__=="__main__":
+    unittest.main()
