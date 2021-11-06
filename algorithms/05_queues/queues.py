e403f21c3dd5fd4a4b45d108070993094eb229e8
Danil Tsygolnik <tsygolnikd@gmail.com>
2021-11-06 13:07:21 +0300

    algo, queues, wip: rotate_queue() - ready, tests ok

--- algorithms/05_queues/queues.py
+++ algorithms/05_queues/queues.py
@@ -6,12 +6,12 @@ class Queue:
 
     def size(self):
         return self.count
- 
+
     def enqueue(self, item):
         """Add an item to queue"""
         self.queue.append(item)
         self.count += 1
- 
+
     def dequeue(self):
         """Return queue head value and delete the item"""
         if self.count > 0:
@@ -20,3 +20,10 @@ class Queue:
             self.count -= 1
             return head
         return None
+
+    def rotate_queue(self, steps):
+        cnt = 0
+        while cnt < steps:
+            item = self.dequeue() # remember removed head
+            self.enqueue(item) # add item to queue again
+            cnt += 1
--- algorithms/05_queues/test_queues.py
+++ algorithms/05_queues/test_queues.py
@@ -29,6 +29,21 @@ class TestQueue(unittest.TestCase):
             cnt += 1
         self.assertIs(self.qu.dequeue(), None)
 
+    def test_rotate(self):
+        self.qu.queue = [1,2,3,4]
+        self.qu.count = 4
+        self.qu.rotate_queue(0)
+        self.assertEqual(self.qu.queue, [1,2,3,4])
+        self.assertEqual(self.qu.size(), 4)
+        self.qu.rotate_queue(1)
+        self.assertEqual(self.qu.queue, [2,3,4,1])
+        self.qu.rotate_queue(2)
+        self.assertEqual(self.qu.queue, [4,1,2,3])
+        self.qu.rotate_queue(5)
+        self.assertEqual(self.qu.queue, [1,2,3,4])
+
+
+
 
 if __name__=="__main__":
     unittest.main()
