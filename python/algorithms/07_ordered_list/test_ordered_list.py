7dd82e3d099a5ebd83daae0c79a8e038d34f3c8b
Danil Tsygolnik <tsygolnikd@gmail.com>
2021-11-16 11:26:20 +0300

    algo, ordered list, wip: dir initialized

---
+++ python/algorithms/07_ordered_list/test_ordered_list.py
@@ -0,0 +1,13 @@
+import unittest
+from ordered_list import OrderedList
+
+class TestOrderedList(unittest.TestCase):
+
+    def setUp(self):
+        self.dq = OrderedList()
+
+    def test_main_methods(self):
+
+
+if __name__=="__main__":
+    unittest.main()
