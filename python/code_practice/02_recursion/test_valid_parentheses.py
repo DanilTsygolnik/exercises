4931e371bcf148c39661e99adb7678e664ae8748
Danil Tsygolnik <tsygolnikd@gmail.com>
2021-12-03 07:34:06 +0300

    recursion, valid_parentheses, wip: 2nd solution, test -- still several issues

--- python/code_practice/02_recursion/test_valid_parentheses.py
+++ python/code_practice/02_recursion/test_valid_parentheses.py
@@ -6,19 +6,32 @@ class TestFunc(unittest.TestCase):
     def test_main(self):
         test_samples = \
         [
-            ("(", False),
-            (")", False),
-            ("   (", False),
-            ("   )", False),
-            (")(", False),
-            ("(()", False),
-            ("((()", False),
-            ("()", True),
-            ("(())", True),
-            ("()()", True),
+            ("test 0", "", True),
+            ("test 1", "(", False),
+            ("test 2", ")", False),
+            ("test 3", "   (", False),
+            ("test 4", "   )", False),
+            ("test 5", ")(", False),
+            ("test 6", "(()", False),
+            ("test 7", "((()", False),
+            ("test 8", "()", True),
+            ("test 9", "(())", True),
+            ("test 10", "()()", True),
+            ("test 11", ")())", False),
+            ("test 12", "((()", False),
+            ("test 13", "((()))", True),
+            ("test 14", "(()())", True),
+            ("test 15", "())(()", False),
+            ("test 16", ")()()(", False),
+            ("test 17", "(((())))", True),
+            ("test 18", "((()()))", True),
+            ("test 19", "(())(())", True),
+            ("test 20", "((()))))", False),
+            ("test 21", "()))((()", False)
         ]
         for i in test_samples:
-            self.assertIs(valid_parentheses(i[0]), i[1])
+            print(i[0])
+            self.assertIs(valid_parentheses(i[1]), i[2])
 
 if __name__ == "__main__":
     unittest.main()
--- python/code_practice/02_recursion/valid_parentheses.py
+++ python/code_practice/02_recursion/valid_parentheses.py
@@ -1,11 +1,42 @@
-def valid_parentheses(string):
+def iter_func(num_curr_step, iter_steps_num, parentheses, valid_num, invalid_num):
 
-    # def recurr_fun(parentheses:list):
-    #     if parentheses == []:
-    #         return True
-    #     if parentheses[0] == "(" and parentheses[-1] == ")":
-    #         return recurr_fun(parentheses[1:-1])
-    #     return False
+    # записываем комбинацию рассматриваемой пары скобок в переменную curr_pair
+    curr_pair = "".join([parentheses[0], parentheses[-1]])
+
+    if num_curr_step == 1:
+        if curr_pair != "()": # проверка внешней пары скобок:
+            return False
+        if iter_steps_num == 1: # скобок всего 2, и они валидные ==> вернуть окончательный ответ
+            return True
+        return iter_func(num_curr_step+1, # переход к сл. итерации \
+                         iter_steps_num, \
+                         parentheses[1:-1], # исключаем из рассмотрения проверенные скобки \
+                         valid_num+1, # обновляем счетчик валидных пар \
+                         invalid_num)
+
+    # действия на втором и последующих шагах итерации
+    if not curr_pair in ["()", ")("]: # если curr_pair не "()" и не ")(":
+        return False # т.к. скобок четное кол-во, то "(" и ")" должно быть строго поровну
+    if curr_pair == "()":
+        valid_num += 1
+    else:
+        invalid_num += 1
+    if num_curr_step != iter_steps_num: # если шаг_итерации не последний:
+        return iter_func(num_curr_step+1, iter_steps_num, parentheses[1:-1], \
+                         valid_num, # текущие значения счетчиков валидных ... \
+                         invalid_num) # ... и невалидных пар
+
+    # иначе, на последнем шаге проводим анализ соотношения числа валидных и невалидных пар
+    if iter_steps_num % 2 == 0: # если при четном числе шагов итерации:
+        if valid_num >= invalid_num:
+            return True
+        return False
+    # иначе, при нечетном числе шагов
+    if valid_num < invalid_num:
+        return False
+    return True
+
+def valid_parentheses(string):
 
     parentheses = []
     parentheses_cnt = 0
@@ -13,11 +44,9 @@ def valid_parentheses(string):
         if c in "()":
             parentheses.append(c)
             parentheses_cnt += 1
-    if parentheses_cnt != 0 and parentheses_cnt % 2 == 0:
-        valid_marker = 1
-        for p in parentheses:
-            if p == ")":
-                valid_marker *= -1
-        if valid_marker == 1:
+    if parentheses_cnt % 2 == 0:
+        if parentheses_cnt == 0:
             return True
+        iter_steps_num = parentheses_cnt // 2
+        return iter_func(1, iter_steps_num, parentheses, 0, 0)
     return False
